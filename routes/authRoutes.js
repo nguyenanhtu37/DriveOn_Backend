c50281f425d2cd8bff259c4460d7bec475d254e3
Hoang Enzo <96958365+hoangenzo11@users.noreply.github.com>
2025-01-15 15:00:20 +0700

    fix model

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
-const { authenticateGarageOwner } = require('../middleware/authMiddleware');
+const { authenticateGarageManager } = require('../middleware/authMiddleware');
 
 router.post('/register/carOwner', authController.registerCarOwner);
-router.post('/register/garageOwner', authController.registerGarageOwner);
-router.post('/register/garageStaff', authenticateGarageOwner, authController.registerGarageStaff);
+router.post('/register/garageManager', authController.registerGarageManager);
+router.post('/register/garageStaff', authenticateGarageManager, authController.registerGarageStaff);
 router.post('/login', authController.login);
 router.get('/verify-email', authController.verifyEmail);
 
