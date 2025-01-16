1a4af053e1ad34341d2e6fb6bf3c0d5924c696a3
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2025-01-17 05:28:11 +0700

    Done delete garage for admin

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import { authMiddleware, adminMiddleware } from '../middleware/authMiddleware.js';
-import { viewGarageRegistrations, approveGarageRegistration, rejectGarageRegistration } from '../controller/adminController.js';
+import { viewGarageRegistrations, approveGarageRegistration, rejectGarageRegistration, deleteGarage } from '../controller/adminController.js';
 
 const router = express.Router();
 
@@ -10,5 +10,6 @@ router.use(adminMiddleware);
 router.get('/garage-registrations', viewGarageRegistrations);
 router.post('/garage-registrations/:id/approve', approveGarageRegistration);
 router.post('/garage-registrations/:id/reject', rejectGarageRegistration);
+router.delete('/garage/:id', deleteGarage);
 
 export default router;
