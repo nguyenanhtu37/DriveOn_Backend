dbd70a9c0ccdf7554f4a1608fc93daca0d5d1696
Ngọc Tâm <ngoctamqb123@gmail.com>
2025-01-19 21:40:53 +0700

    fix/TamTVN/update register garage

--- controller/managerController.js
+++ controller/managerController.js
@@ -1,26 +1,32 @@
-import Garage from '../models/garage.js';
+import Garage from "../models/garage.js";
 
 const registerGarage = async (req, res) => {
-    const { name, address, phone, description, workingHours, coinBalance, user } = req.body;
-    try {
-        // tao moi garage, default pending
-        const newGarage = new Garage({
-            name,
-            address,
-            phone,
-            description,
-            workingHours,
-            coinBalance,
-            user,
-            status: "pending",
-            createdAt: new Date(),
-            updatedAt: new Date()
-        });
-        await newGarage.save();
-        res.status(200).json({ message: "Garage registration submitted successfully", garage: newGarage });
-    } catch (err) {
-        res.status(500).json({ error: err.message });
-    }
+  const user = req.user;
+  console.log("User information:", user);
+  const { name, address, phone, description, workingHours, coinBalance } =
+    req.body;
+  try {
+    // tao moi garage, default pending
+    const newGarage = new Garage({
+      name,
+      address,
+      phone,
+      description,
+      workingHours,
+      coinBalance,
+      user: [user.id],
+      status: "pending",
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    });
+    await newGarage.save();
+    res.status(200).json({
+      message: "Garage registration submitted successfully",
+      garage: newGarage,
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 };
 
-export {registerGarage};
+export { registerGarage };
