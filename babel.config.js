031d3d1a03d2bf2a92eebe435c834a5217b2fe4b
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2025-02-19 00:44:38 +0700

    Added validations for all functions sprint 2 belongs to TuNA

---
+++ babel.config.js
@@ -0,0 +1,7 @@
+export default {
+    presets: [
+      '@babel/preset-env',  // Chuyển đổi mã JavaScript ES6+ thành ES5 để Jest có thể hiểu
+      '@babel/preset-modules'  // Hỗ trợ cho các module ECMAScript (ESM)
+    ],
+  };
+  
