d7deff7d5ed70c9acfde52ded31e2786a707061e
Khang Nguyen <khangnvde170679@fpt.edu.vn>
2025-02-19 09:55:23 +0700

    save changes before pull

--- routes/vehicleRoutes.js
+++ routes/vehicleRoutes.js
@@ -7,7 +7,7 @@ const router = express.Router();
 router.use(authMiddleware);
 // router.use(carOwnerMiddleware);
 
-router.post('/add', addVehicle);
+router.post('/add',addVehicle);
 router.get('/', viewVehicles);
 router.get('/:id', getVehicleById); // Tất cả các role đều có thể xem chi tiết một vehicle
 router.put('/:id', updateVehicle); 
