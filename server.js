05899dbe547a05bc4630a32b71c35961bbd15240
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2025-03-11 01:10:02 +0700

    Deploy 1

--- server.js
+++ server.js
@@ -38,10 +38,8 @@ app.use(
 );
 
 app.use((req, res, next) => {
-  res.removeHeader("Cross-Origin-Opener-Policy");
-  res.removeHeader("Cross-Origin-Embedder-Policy");
-  res.setHeader("Cross-Origin-Opener-Policy", "same-origin");
-  res.setHeader("Cross-Origin-Embedder-Policy", "require-corp");
+  res.setHeader("Cross-Origin-Opener-Policy", "unsafe-none");
+  res.setHeader("Cross-Origin-Embedder-Policy", "credentialless");
   res.setHeader("Content-Security-Policy", "frame-ancestors 'self' https://accounts.google.com");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
