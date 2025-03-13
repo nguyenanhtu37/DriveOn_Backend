9ac95501ed93d24619317c35c04addc6aaddb51d
Hoang Enzo <96958365+hoangenzo11@users.noreply.github.com>
2025-03-13 10:33:42 +0700

    view appointment detail

--- src/routes/appointmentRoutes.js
+++ src/routes/appointmentRoutes.js
@@ -6,6 +6,6 @@ const router = express.Router();
 
 router.post("/service-detail/:serviceDetailId/create", authMiddleware, createAppointment); // Create appointment
 router.get("/view-list-user-appointment", authMiddleware, getAppointmentsByUser); // Get appointments by user
-// router.get("/:appointmentId", authMiddleware, getAppointmentById); // Get specific appointment by ID
+router.get("/:appointmentId", authMiddleware, getAppointmentById); // Get specific appointment by ID
 
 export default router;
