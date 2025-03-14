f8f39646fb431325f67d68065a5e0ea197c8423d
Nguyen Anh Tu <nguyenanhtu3703@gmail.com>
2025-03-14 23:44:59 +0700

    CRUD car brand

--- src/routes/brandRoutes.js
+++ src/routes/brandRoutes.js
@@ -1,9 +1,12 @@
 import express from "express";
-import { addBrand } from "../controller/brandController.js";
+import { addBrand, getBrands, updateBrand, deleteBrand } from "../controller/brandController.js";
 import { adminMiddleware } from "../middleware/adminMiddleware.js";
 
 const router = express.Router();
 
 router.post('/add', adminMiddleware, addBrand); // add new brand 
+router.get('/get', getBrands); // get all brands
+router.put('/update', adminMiddleware, updateBrand); // update brand
+router.delete('/delete', adminMiddleware, deleteBrand); // delete brand
 
 export default router;
--- src/service/brandService.js
+++ src/service/brandService.js
@@ -1,7 +1,13 @@
 import Brand from "../models/brand.js";
+import { brandSchema } from "../validator/brandValidator.js";
 
 const addBrand = async (brandName, logo) => {
   try {
+    brandSchema.parse({brandName, logo});
+    const brand_in_DB = await Brand.findOne({brandName});
+    if (brand_in_DB) {
+      throw new Error(`Brand ${brandName} already exists!`);
+    }
     const newBrand = new Brand({ brandName, logo });
     await newBrand.save();
     return newBrand;
@@ -10,4 +16,46 @@ const addBrand = async (brandName, logo) => {
   }
 };
 
-export { addBrand };
+const getBrands = async (req, res) => {
+  try {
+    const brandList = await Brand.find();
+    return brandList;
+  } catch (err) {
+    throw new Error("Cannot get brand list!");
+  }
+};
+
+const updateBrand = async (brandId, brandName, logo) => {
+  try {
+    if (!brandId) {
+      throw new Error("Invalid brand!");
+    }
+    brandSchema.parse({brandName, logo});
+    const updatedBrand = await Brand.findByIdAndUpdate(
+      brandId,
+      { brandName, logo },
+      { new: true } 
+    );
+    if (!updatedBrand) {
+      throw new Error("Brand not found!");
+    }
+    return updatedBrand;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+const deleteBrand = async (brandId) => {
+  try {
+    if (!brandId) {
+      throw new Error("Invalid brand!");
+    };
+    
+    const result = await Brand.findByIdAndDelete(brandId);
+    return result;
+  } catch (err) {
+    throw new Error(err.message);
+  }
+};
+
+export { addBrand, getBrands, updateBrand, deleteBrand };
